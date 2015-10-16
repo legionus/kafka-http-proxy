059f9c759c0dd47b59aa4c63f0615acbe91c41dd
Alexey Gladkov <gladkov.alexey@gmail.com>
2015-10-16 13:58:16 +0300

    Increase timeouts
    Signed-off-by: Alexey Gladkov <gladkov.alexey@gmail.com>

--- config.go
+++ config.go
@@ -81,26 +81,26 @@ func (c *Config) SetDefaults() {
 	c.Global.Pidfile = "/run/kafka-http-proxy.pid"
 
 	c.Metadata.CacheTimeout.Duration = 3 * time.Second
-	c.Metadata.ReadTimeout.Duration = 500 * time.Millisecond
+	c.Metadata.ReadTimeout.Duration = 1 * time.Second
 
 	c.Broker.NumConns = 100
 	c.Broker.DialTimeout.Duration = 500 * time.Millisecond
 	c.Broker.LeaderRetryLimit = 2
 	c.Broker.LeaderRetryWait.Duration = 500 * time.Millisecond
 	c.Broker.ReconnectTimeout.Duration = 15 * time.Second
-	c.Broker.ReadTimeout.Duration = 100 * time.Millisecond
+	c.Broker.ReadTimeout.Duration = 10 * time.Second
 
 	c.Producer.RequestTimeout.Duration = 5 * time.Second
 	c.Producer.RetryLimit = 2
 	c.Producer.RetryWait.Duration = 200 * time.Millisecond
-	c.Producer.WriteTimeout.Duration = 500 * time.Millisecond
+	c.Producer.WriteTimeout.Duration = 15 * time.Second
 
 	c.Consumer.RequestTimeout.Duration = 50 * time.Millisecond
 	c.Consumer.RetryLimit = 2
 	c.Consumer.RetryWait.Duration = 50 * time.Millisecond
 	c.Consumer.RetryErrLimit = 2
 	c.Consumer.RetryErrWait.Duration = 50 * time.Millisecond
-	c.Consumer.ReadTimeout.Duration = 0
+	c.Consumer.ReadTimeout.Duration = 15 * time.Second
 	c.Consumer.MinFetchSize = 1
 	c.Consumer.MaxFetchSize = 4194304
 	c.Consumer.DefaultFetchSize = 524288
